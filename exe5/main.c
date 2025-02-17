3ec5b2666ec709acec23bc76162b918f355e24fd
MatheusGimenesDeAlmeida <mthga500@gmail.com>
2025-02-17 12:59:13 -0300

    Ex5

--- exe5/main.c
+++ exe5/main.c
@@ -5,20 +5,6 @@
 const int BTN_PIN = 26;
 const int BTN_PIN_2 = 7;
 
-
-bool debounce_button(int pin) {
-    if (!gpio_get(pin)) {  
-        sleep_ms(50);  
-        if (!gpio_get(pin)) {  
-            while (!gpio_get(pin)) {  
-                sleep_ms(10);
-            }
-            return true;  
-        }
-    }
-    return false;  
-}
-
 int main() {
     stdio_init_all();
 
@@ -29,18 +15,20 @@ int main() {
     gpio_init(BTN_PIN_2);
     gpio_set_dir(BTN_PIN_2, GPIO_IN);
     gpio_pull_up(BTN_PIN_2);
-
     int cnt_1 = 0;
     int cnt_2 = 0;
 
     while (true) {
-        if (debounce_button(BTN_PIN)) {
-            printf("Botao 1: %d\n", cnt_1++);
-        }
-        if (debounce_button(BTN_PIN_2)) {
-            printf("Botao 2: %d\n", cnt_2++);
-        }
-    }
 
-    return 0;
+        if (!gpio_get(BTN_PIN)) {
+            printf("Botao 1: %d\n", cnt_1++);
+            sleep_ms(300);
+            }
+        
+        if (!gpio_get(BTN_PIN_2)) {
+            printf("Botao 2: %d\n", cnt_2++);
+            sleep_ms(300);
+        }
+    } return 0;
 }
+    
