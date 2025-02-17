4ef3c39ee7603830eac44cd31ca444964d332c69
MatheusGimenesDeAlmeida <mthga500@gmail.com>
2025-02-17 12:06:39 -0300

    Ex5

--- exe5/main.c
+++ exe5/main.c
@@ -5,6 +5,20 @@
 const int BTN_PIN = 26;
 const int BTN_PIN_2 = 7;
 
+
+bool debounce_button(int pin) {
+    if (!gpio_get(pin)) {  
+        sleep_ms(50);  
+        if (!gpio_get(pin)) {  
+            while (!gpio_get(pin)) {  
+                sleep_ms(10);
+            }
+            return true;  
+        }
+    }
+    return false;  
+}
+
 int main() {
     stdio_init_all();
 
@@ -15,38 +29,18 @@ int main() {
     gpio_init(BTN_PIN_2);
     gpio_set_dir(BTN_PIN_2, GPIO_IN);
     gpio_pull_up(BTN_PIN_2);
+
     int cnt_1 = 0;
     int cnt_2 = 0;
-    int apertado_1 = 0;
-    int apertado_2 = 0;
 
     while (true) {
-        if (!gpio_get(BTN_PIN)) {
-          if (apertado_1 == 1) {
-            cnt_1 ++
+        if (debounce_button(BTN_PIN)) {
             printf("Botao 1: %d\n", cnt_1++);
-            apertado_1 -= 1;
-            }
-          else {
-            cnt_1 ++
-            printf("Botao 1: %d\n", cnt_1++);
-            apertado_1 += 1;
-            }
-        sleep_ms(250);
         }
-
-        if (!gpio_get(BTN_PIN_2)) {
-          if (apertado_2 == 1) {
-            cnt_2 ++
+        if (debounce_button(BTN_PIN_2)) {
             printf("Botao 2: %d\n", cnt_2++);
-            apertado_2 -= 1;
-            }
-          else {
-            cnt_2 ++
-            printf("Botao 2: %d\n", cnt_2++);
-            apertado_2 += 1;
-            }
-        sleep_ms(250);
         }
     }
+
+    return 0;
 }
