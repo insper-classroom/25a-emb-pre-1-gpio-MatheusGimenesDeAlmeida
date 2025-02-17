ade352ff481ee15a50fbd9edb0bff6b5c3c3f5b2
MatheusGimenesDeAlmeida <mthga500@gmail.com>
2025-02-17 10:12:34 -0300

    ex 3

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,58 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-int main() {
-  stdio_init_all();
+const int BTN_PIN_RED = 28;
+const int BTN_PIN_GREEN  = 26;
+const int LED_PIN_RED = 4;
+const int LED_PIN_GREEN = 6; 
 
-  while (true) {
-  }
+int main() {
+    stdio_init_all();
+
+    gpio_init(BTN_PIN_GREEN);
+    gpio_init(BTN_PIN_RED);
+    gpio_init(LED_PIN_RED);
+    gpio_init(LED_PIN_GREEN);
+
+    gpio_set_dir(BTN_PIN_GREEN, GPIO_IN);
+    gpio_set_dir(BTN_PIN_RED, GPIO_IN);
+    gpio_set_dir(LED_PIN_RED, GPIO_OUT);
+    gpio_set_dir(LED_PIN_GREEN, GPIO_OUT);
+
+    gpio_pull_up(BTN_PIN_GREEN);
+    gpio_pull_up(BTN_PIN_RED);
+
+    int apertado_red = 0;
+    int apertado_green = 0;
+
+    while (true) {
+        if (!gpio_get(BTN_PIN_RED)) {
+            if (apertado_red == 1) {
+                gpio_put(LED_PIN_RED, 0);
+                printf("Desligando LED vermelho\n");
+                apertado_red -= 1;
+            }
+            else {
+                printf("Ligando LED vermelho\n");
+                gpio_put(LED_PIN_RED, 1);
+                apertado_red += 1;
+            }
+            sleep_ms(250);
+        }
+
+        if (!gpio_get(BTN_PIN_GREEN)) {
+            if (apertado_green == 1) {
+                gpio_put(LED_PIN_GREEN, 0);
+                printf("Desligando LED verde\n");
+                apertado_green -=1;
+            }
+            else {
+                printf("Ligando LED verde\n");
+                gpio_put(LED_PIN_GREEN, 1);
+                apertado_green += 1;
+            }
+            sleep_ms(250);
+        }
+    }
+    return 0; 
 }
