7082142973276f5e7e44580ee6fb6e9bb8a1fa38
MatheusGimenesDeAlmeida <mthga500@gmail.com>
2025-02-17 10:12:43 -0300

    ex 4

--- exe4/main.c
+++ exe4/main.c
@@ -2,10 +2,44 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-int main() {
-  stdio_init_all();
+const int BTN_PIN = 28;
+const int LED_PIN_RED = 5;
+const int LED_PIN_PP = 8; 
+const int LED_PIN_BLU = 11;
+const int LED_PIN_YE = 15;
 
-  while (true) {
-    // Use delay de 300 ms entre os estados!
-  }
+int main() {
+    stdio_init_all();
+
+    gpio_init(BTN_PIN);
+    gpio_init(LED_PIN_RED);
+    gpio_init(LED_PIN_PP);
+    gpio_init(LED_PIN_BLU);
+    gpio_init(LED_PIN_YE);
+
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_set_dir(LED_PIN_RED, GPIO_OUT);
+    gpio_set_dir(LED_PIN_PP, GPIO_OUT);
+    gpio_set_dir(LED_PIN_BLU, GPIO_OUT);
+    gpio_set_dir(LED_PIN_YE, GPIO_OUT);
+
+    gpio_pull_up(BTN_PIN);
+
+    while (true) {
+        if (!gpio_get(BTN_PIN)) {
+          gpio_put(LED_PIN_RED, 1);
+          sleep_ms(300);
+          gpio_put(LED_PIN_RED, 0);
+          gpio_put(LED_PIN_PP, 1);
+          sleep_ms(300);
+          gpio_put(LED_PIN_PP, 0);
+          gpio_put(LED_PIN_BLU, 1);
+          sleep_ms(300);
+          gpio_put(LED_PIN_BLU, 0);
+          gpio_put(LED_PIN_YE, 1);
+          sleep_ms(300);
+          gpio_put(LED_PIN_YE, 0);
+        }
+        sleep_ms(50);}
+    return; 
 }
